bbcb40e47ed2531fd32f1f9e73bada802b789dba
Andrew Davis <atd285@gmail.com>
2023-03-03 15:59:45 -0500

    prisma query with each loop

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,26 +3,7 @@ import type { PageServerLoad } from './$types';
 
 const prisma = new PrismaClient();
 
-async function main() {
-	const user = await prisma.user.create({
-		data: {
-			name: 'andrew'
-		}
-	});
-
-	console.log(user);
-}
-
-export const load = (async ({ params }) => {
-	main()
-		.then(async () => {
-			await prisma.$disconnect();
-		})
-		.catch(async (e) => {
-			console.error(e);
-
-			await prisma.$disconnect();
-
-			process.exit(1);
-		});
-}) satisfies PageServerLoad;
+export const load = (async () => {
+  const users = await prisma.user.findMany();
+  return {users}
+}) satisfies PageServerLoad
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+      console.log(data);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1>hello world</h1>
 
+<div>
+     {#each data.users as {name}}
+          <p>{name}</p>
+     {/each}
 
-<script>
- 
-</script>
+</div>
